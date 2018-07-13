0a7843d0d1f305496470799ba0cce37430cbd2d4
neyer <neyerolmos@gmail.com>
2018-07-13 09:51:32 -0400

    first commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>hello</title>
+<style>
+body{
+  text-align: center;
+}
+</style>
+  </head>
+  <body>
+  <h1>Hello World</h1>
+  </body>
+</html>
